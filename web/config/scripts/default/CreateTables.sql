98a0cf223b798189074a8980bc551f6fe475e4b2
chrissco <chrissgo24@gmail.com>
2014-11-03 23:40:39 +0100

    added latitude & longtitude to photo Table

--- web/config/scripts/default/CreateTables.sql
+++ web/config/scripts/default/CreateTables.sql
@@ -29,7 +29,9 @@ CREATE TABLE photos (
 	status integer,
 	praise_sum integer,
 	no_votes integer,
-	creation_time bigint
+	creation_time bigint,
+	latitude decimal,
+	longtitude decimal
 );
 
 CREATE TABLE tags (
